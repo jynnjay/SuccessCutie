57bf44ca8ae87087e0c1b38bfac71f33b1858cf1
Jay-Ann <85058488+jynnjay@users.noreply.github.com>
2022-04-30 23:21:49 +0800

    Update index.js

--- website/static/index.js
+++ website/static/index.js
@@ -1,8 +1,1 @@
-function deleteNote(noteId) {
-  fetch("/delete-note", {
-    method: "POST",
-    body: JSON.stringify({ noteId: noteId }),
-  }).then((_res) => {
-    window.location.href = "/";
-  });
-}
+
