0462a059bc0b04858552e3908b18aec6c00506c8
Jay-Ann <85058488+jynnjay@users.noreply.github.com>
2022-04-30 23:07:20 +0800

    Add files via upload

--- website/auth.py
+++ website/auth.py
@@ -21,9 +21,9 @@ def login():
                 login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
-                flash('Incorrect password, try again.', category='error')
+                flash('Invalid email or password.', category='error')
         else:
-            flash('Email does not exist.', category='error')
+            flash('Invalid email or password.', category='error')
 
     return render_template("login.html", user=current_user)
 
@@ -40,6 +40,7 @@ def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
         first_name = request.form.get('firstName')
+        last_name = request.form.get('lastName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
@@ -50,12 +51,14 @@ def sign_up():
             flash('Email must be greater than 3 characters.', category='error')
         elif len(first_name) < 2:
             flash('First name must be greater than 1 character.', category='error')
+        elif len(last_name) < 2:
+            flash('Last name must be greater than 1 character.', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+            new_user = User(email=email, first_name=first_name, last_name=last_name, password=generate_password_hash(
                 password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
